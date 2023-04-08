0f00582292f2b99068b0b6e2c71eafd3fca9dd40
Leonardo <leonarddo.gomees@gmail.com>
2023-04-08 16:39:21 -0300

    incrementando a aplicação catalogo de streaming

---
+++ CatalogoDeStreaming/src/br/com/estudos/catalogoDeFilmes/calculos/Classificavel.java
@@ -0,0 +1,6 @@
+package br.com.estudos.catalogoDeFilmes.calculos;
+
+public interface Classificacao {
+
+    public int getClassificacao();
+}
---
+++ CatalogoDeStreaming/src/br/com/estudos/catalogoDeFilmes/model/Epsodio.java
@@ -0,0 +1,53 @@
+package br.com.estudos.catalogoDeFilmes.model;
+
+import br.com.estudos.catalogoDeFilmes.calculos.Classificavel;
+
+public class Epsodios implements Classificavel {
+
+    private int numero;
+    private String nome;
+    private Serie serie;
+    private int totalVisualizacoes;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTotalVisualizacoes() {
+        return totalVisualizacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public void setTotalVisualizacoes(int totalVisualizacoes) {
+        this.totalVisualizacoes = totalVisualizacoes;
+    }
+
+    @Override
+    public int getClassificacao() {
+
+        if(totalVisualizacoes > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
+    }
+}
