4e09f04bdc29cfb89dd751f8166a1b0791a5af4e
Leonardo <leonarddo.gomees@gmail.com>
2023-05-02 21:31:57 -0300

    estudando java

--- CatalogoDeStreaming/src/br/com/estudos/catalogoDeFilmes/principal/PrincipalComBusca.java
+++ CatalogoDeStreaming/src/br/com/estudos/catalogoDeFilmes/principal/PrincipalComBusca.java
@@ -1,11 +1,6 @@
 package br.com.estudos.catalogoDeFilmes.principal;
 
 import java.io.FileWriter;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,7 +9,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import br.com.estudos.catalogoDeFilmes.Exception.ErroDeConversaoDeAnoException;
+import br.com.estudos.catalogoDeFilmes.model.ConsultaConteudo;
 import br.com.estudos.catalogoDeFilmes.model.Conteudo;
 import br.com.estudos.catalogoDeFilmes.model.ConteudoOMDB;
 
@@ -22,68 +17,38 @@ public class PrincipalComBusca {
 
     public static void main(String[] args) throws Exception {
 
-        String apiInicio = "http://www.omdbapi.com/?t=";
-        String apiKey = "&apikey=c6af06db";
-        String busca = "";
+        
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .setPrettyPrinting()
-                .create();
         List<Conteudo> conteudos = new ArrayList<>();
 
-        Scanner scanner = new Scanner(System.in);        
+        Scanner scanner = new Scanner(System.in);
+        ConsultaConteudo consulta = new ConsultaConteudo(); 
+        
+        
 
-        while (!busca.equalsIgnoreCase("sair")) {
+        //while (!conteudo.title().equalsIgnoreCase("sair")) {
             System.out.println("Qual o filme você quer buscar? ");
+
+            ConteudoOMDB conteudo = consulta.buscaConteudo(scanner.nextLine());
+
+            System.out.println(conteudo);
             
-            busca = scanner.nextLine();
 
-            if (busca.equalsIgnoreCase("sair")) {
-                break;
-            }
+            //if (conteudo.title().equalsIgnoreCase("sair")) {
+           //     break;
+           // }
 
-            String endereco = apiInicio + busca.replace(" ", "+") + apiKey;
+            
 
-            try {
+            
+       // }
 
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(endereco))
-                        .build();
-
-                HttpResponse<String> response = client
-                        .send(request, BodyHandlers.ofString());
-                String json = response.body();
-
-                ConteudoOMDB conteudoOMDB = gson.fromJson(json, ConteudoOMDB.class);
-
-                Conteudo conteudo = new Conteudo(conteudoOMDB);
-                System.out.println(conteudo);
-                conteudos.add(conteudo);
-
-            } catch (NumberFormatException e) {
-
-                System.out.println("Aconteceu um erro: ");
-                System.out.println(e.getMessage());
-
-            } catch (IllegalArgumentException i) {
-                System.out.println("Algum erro de argumento na busca, verifique o endereço");
-
-            } catch (ErroDeConversaoDeAnoException e) {
-
-                System.out.println(e.getMessage());
-
-            }
-
-        }
-
-        System.out.println(conteudos);
+       /* System.out.println(conteudos);
 
         FileWriter escrita = new FileWriter("conteudo.json");
         escrita.write(gson.toJson(conteudos));
         escrita.close();
-        System.out.println("Programa finalizado");
+        System.out.println("Programa finalizado");*/
 
     }
 
